3cb17014ca9b64d218b37506fdac244f6e08e401
Spencer Barnes <SWilliamBarnes@gmail.com>
2019-12-19 00:35:20 -0600

    Card links can work, to some degree at least

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ app = Flask(__name__,
             static_folder='web/static',
             template_folder='web/templates')
 
-GAMES = ["first", "second", "third", "fourth", "fifth"]
+GAMES = ["helloWorld", "first", "second", "third", "fourth"]
 
 @app.route("/")
 def loadHomePage():
     return render_template("index.html", cards=GAMES)
+
+@app.route("/<string:page_name>")
+def loadPage(page_name):
+    return render_template(page_name+".html")
 
 if __name__ == "__main__":
     app.run()
