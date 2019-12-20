94bbbffd9fb3239b4308b8e0107d5e05b5edf95a
Spencer Barnes <SWilliamBarnes@gmail.com>
2019-12-20 04:05:49 -0600

    Coin flips every time now, the correct number of times
    The coin always flips and flips the correct number of times, but does not always land on the correct face

--- web/static/js/coinFlipper.js
+++ web/static/js/coinFlipper.js
@@ -1,27 +1,45 @@
 // coinFlipper.js
 // Helper functions for the coin flipping game
 
+// Necessary evil to know coin's state between clicks
+var oldResult = 1;
+
 function getFlipResult()
 {
+    // Random outputs 0<x<1, scale up then floor to 0 or 1
     let result = Math.floor(Math.random()*2);
     return result;
+}
+
+function resultToString(result)
+{
+    // "Magic" strings... have to arbitrarily define this somewhere
+    if (result != 0)
+        return "Heads";
+    return "Tails";
 }
 
 function flip(coin, resultDisplay)
 {
     let result = getFlipResult();
-    let resultString = "";
+    // Clone element to be reflowed/refreshed
+    let newCoin = coin.cloneNode(true);
 
-    if (result != 0)
+    if (result != oldResult)
     {
-        coin.setAttribute("class", "coin animate-heads");
-        resultString = "Heads";
+        // Flip 180
+        newCoin.setAttribute("class", "coin animate-opposite");
     }
     else
     {
-        coin.setAttribute("class", "coin animate-tails");
-        resultString = "Tails"; 
+        // Flip 360
+        newCoin.setAttribute("class", "coin animate-duplicate");
     }
+    
+    // Reflow element so animation runs again
+    coin.parentNode.replaceChild(newCoin, coin);
 
-    resultDisplay.textContent = resultString;
+    resultDisplay.textContent = resultToString(result);
+    console.log("old: "+oldResult+" cur: "+result);
+    oldResult = result;
 }
