4b57bfda1447fe3993dd9a31185b8c330834f8f5
Spencer Barnes <interstellarjumper@gmail.com>
2020-05-28 16:04:25 -0500

    Move flask server

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__,
             static_url_path='', 
-            static_folder='web/static',
-            template_folder='web/templates')
+            static_folder='../web/static',
+            template_folder='../web/templates')
 
 GAMES = ["helloWorld", "coinFlipper"]
 
